e1a685505eb8e4bb92f975f0547b23a6d629251a
Xpinux <Xpinux@192.168.57.1>
2017-12-09 10:49:26 +0200

    Revision 07 Existing Customer 01 Added:Full First version of existing Done 1/2

--- src/Cashier/Cashier_Matches.java
+++ src/Cashier/Cashier_Matches.java
@@ -346,7 +346,11 @@ public String User1;
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
+         
+            Cashier_BookForm_ExistingCustomer Window2 = new Cashier_BookForm_ExistingCustomer(User1,jList1.getSelectedValue());
+            Window2.setVisible(true);
+            setVisible(false);
+       
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void btn_MatchesMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btn_MatchesMouseEntered
